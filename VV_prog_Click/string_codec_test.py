2e4571bca71a199d20d8039902b461291e35f8b2
mortenl <morten.larsen@7sense.no>
2020-08-07 10:50:29 +0200

    Added: - more tests - decoding functionality split out as 'decode_byte_arr()' function - argument 'utf_encode' to 'run_exe_file()' (and passedon to function above)

--- VV_prog_Click/string_codec_test.py
+++ VV_prog_Click/string_codec_test.py
@@ -9,24 +9,10 @@ def resource_path(relative_path: str) -> str:
     return os.path.join(base_path, relative_path)
 
 
-def run_exe_file(exe_file_name, verbose=True):
-    SUBPROC_RETVAL_STATUS_SUCCESS = 0
-    status = False
-    #
-    exe_file = resource_path(exe_file_name)
-    #
-    print(f"Running: {exe_file}")
-    try:
-        p1 = subprocess.Popen(exe_file, stdout=subprocess.PIPE)
-        # Run the command
-        output = p1.communicate(timeout=30)[0]
-    except subprocess.TimeoutExpired:
-        print(f"ERROR: timeout from running '{exe_file}'")
-        return status
-
-    lines = output.splitlines()
+def decode_byte_arr(data: bytes, utf_encode: bool = False, verbose: bool = True) -> str:
+    lines_in = data.splitlines()
     lines_out = []
-    for line in lines:
+    for line in lines_in:
         try:
             line_str = line.decode('ascii')
         except UnicodeDecodeError as e:
@@ -41,10 +27,32 @@ def run_exe_file(exe_file_name, verbose=True):
                     print(f"Exception trying to decode as ISO LATIN-1: {e}")
                     line_str = "..."
         finally:
+            if utf_encode:
+                # Ensure output string is UTF-8:
+                line_str = line_str.encode('utf-8').decode('utf-8')
             lines_out.append(line_str)
         if verbose:
             print(line_str)
+    #
+    return lines_out
 
+
+def run_exe_file(exe_file_name, utf_encode: bool = False, verbose: bool = True):
+    SUBPROC_RETVAL_STATUS_SUCCESS = 0
+    status = False
+    #
+    exe_file = resource_path(exe_file_name)
+    #
+    print(f"Running: {exe_file}")
+    try:
+        p1 = subprocess.Popen(exe_file, stdout=subprocess.PIPE)
+        # Run the command
+        output = p1.communicate(timeout=30)[0]
+    except subprocess.TimeoutExpired:
+        print(f"ERROR: timeout from running '{exe_file}'")
+        return status
+
+    lines_out = decode_byte_arr(output, utf_encode=utf_encode)
     status = (p1.returncode == SUBPROC_RETVAL_STATUS_SUCCESS)
     #
     return status, lines_out
@@ -59,4 +67,16 @@ if __name__ == "__main__":
     #
     status, outp = run_exe_file('streng_codec.exe')
     print(f"Status: {status}\nOutput: {outp}")
+    #
+    status, outp = run_exe_file('streng_codec.exe', utf_encode=True)
+    print(f"Status: {status}\nOutput: {outp}")
+    #
+    # Tests directly on non-UTF8 byte-array data:
+    byte_str = ['\xF8', '\xF8', '\xF8', '\xF8', '\xF8']
+    print(f"{byte_str}")
+    print(str(byte_str))
+    byte_arr = bytes([0xF8, 0xF8, 0xF8, 0xF8, 0xF8])
+    print(decode_byte_arr(byte_arr))
+    print(byte_arr.decode('latin1').encode('utf-8').decode('utf-8'))
+
 
